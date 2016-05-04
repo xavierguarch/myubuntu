3aa253d62e638952aef0b7104b92baa36cd7d353
Jordi Íñigo <jig@safelayer.com>
2016-05-04 22:15:24 +0200

    install dockerfile-mode on emacs

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -2,6 +2,12 @@
 (add-to-list 'package-archives
    '("melpa" . "http://melpa.milkbox.net/packages/") t)
 
+; From https://github.com/spotify/dockerfile-mode below
+(add-to-list 'load-path "~/.emacs.d/dockerfile-mode")
+(require 'dockerfile-mode)
+(add-to-list 'auto-mode-alist '("Dockerfile\\'" . dockerfile-mode))
+
+; From http://tleyden.github.io/blog/2014/05/22/configure-emacs-as-a-go-editor-from-scratch/ below
 (add-to-list 'load-path "~/.emacs.d/golang")
 (require 'go-mode-autoloads)
 
@@ -10,5 +16,4 @@
 (setq auto-save-file-name-transforms
   `((".*" ,temporary-file-directory t)))
 
-; From http://tleyden.github.io/blog/2014/05/22/configure-emacs-as-a-go-editor-from-scratch/ below
 
