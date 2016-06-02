11ac1519257b713cb753809eb29e43a6025567c6
Jose Manuel Rivera <jose.manuel.rivera@safelayer.com>
2016-06-02 11:43:07 +0200

    Added Java install script

---
+++ install-java.sh
@@ -0,0 +1,34 @@
+java() {
+  # Use colors, but only if connected to a terminal, and that terminal
+  # supports them.
+  if which tput >/dev/null 2>&1; then
+      ncolors=$(tput colors)
+  fi
+  if [ -t 1 ] && [ -n "$ncolors" ] && [ "$ncolors" -ge 8 ]; then
+    RED="$(tput setaf 1)"
+    GREEN="$(tput setaf 2)"
+    YELLOW="$(tput setaf 3)"
+    BLUE="$(tput setaf 4)"
+    BOLD="$(tput bold)"
+    NORMAL="$(tput sgr0)"
+  else
+    RED=""
+    GREEN=""
+    YELLOW=""
+    BLUE=""
+    BOLD=""
+    NORMAL=""
+  fi
+
+  # Only enable exit-on-error after the non-critical colorization stuff,
+  # which may fail on systems lacking tput or terminfo
+  set -e
+
+  printf "${BLUE}Installing JAVA PPA...${NORMAL}\n"
+  sudo add-apt-repository ppa:webupd8team/java -y
+  sudo apt-get update
+  printf "${BLUE}Installing JAVA...${NORMAL}\n"
+  sudo echo "oracle-java8-installer shared/accepted-oracle-license-v1-1 select true" | sudo debconf-set-selections
+  sudo apt-get install oracle-java8-set-default -y
+  }
+java
