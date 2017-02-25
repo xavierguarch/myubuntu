c6a229bd2c8c50d3a77bdbb1722f3d0d198b8187
Jordi Íñigo <jig@users.noreply.github.com>
2017-02-25 17:03:20 +0100

    Bumped Go to 1.8(.0)

--- install-golang.sh
+++ install-golang.sh
@@ -27,7 +27,7 @@ golang() {
   ###################################
   # Golang
 
-  VERSION=1.7.4
+  VERSION=1.8
 
   printf "${BLUE}Installing Go...${NORMAL}\n"
   wget https://storage.googleapis.com/golang/go$VERSION.linux-amd64.tar.gz -O /tmp/golang.tgz
