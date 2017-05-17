a831fff16994960bd4a08ac82564cb11f620c959
Jordi Íñigo <jig@safelayer.com>
2017-05-17 22:02:46 +0200

    Added kubectl and kops

---
+++ install-kops.sh
@@ -0,0 +1,43 @@
+install_kops() {
+  # Use colors, but only if connected to a terminal, and that terminal
+  # supports them.
+  if which tput >/dev/null 2>&1; then
+      ncolors=$(tput colors)
+  fi
+  if [ -t 1 ] && [ -n "$ncolors" ] && [ "$ncolors" -ge 8 ]; then
+    RED="$(tput setaf 1)"
+    GREEN="$(tput setaf 2)"
+    YELLOW="$(tput setaf 3)"
+    BLUE="$(tput setaf 4)"
+    BOLD="$(tput bold)"
+    NORMAL="$(tput sgr0)"
+  else
+    RED=""
+    GREEN=""
+    YELLOW=""
+    BLUE=""
+    BOLD=""
+    NORMAL=""
+  fi
+
+  # Only enable exit-on-error after the non-critical colorization stuff,
+  # which may fail on systems lacking tput or terminfo
+  set -e
+ 
+  ###################################
+  # kubectl & kops
+  printf "${BLUE}Installing Nodejs...${NORMAL}\n"
+  sudo apt-get -y install curl
+  curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
+  chmod +x ./kubectl
+  sudo mv ./kubectl /usr/local/bin/kubectl
+  echo "source <(kubectl completion bash)" >> ~/.bashrc
+  echo "source <(kubectl completion zsh)" >> ~/.zshrc  
+  
+  curl -LO https://github.com/kubernetes/kops/releases/download/1.6.0/kops-linux-amd64
+  chmod +x ./kops-linux-amd64
+  sudo mv ./kops-linux-amd64 /usr/local/bin/kops
+}
+
+# Check if reboot is needed
+install_kops
