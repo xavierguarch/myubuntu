edd5e327e196e7b83b597da4b9a00e041bb5262f
Jordi Íñigo <jig@users.noreply.github.com>
2017-09-13 17:07:20 +0200

    Updated docker-machine to 0.12.2 (current)

--- install-docker.sh
+++ install-docker.sh
@@ -42,7 +42,7 @@ minimal() {
   sudo chmod +x /usr/local/bin/docker-compose
 
   printf "${BLUE}Installing docker-machine...${NORMAL}\n"
-  curl -L https://github.com/docker/machine/releases/download/v0.7.0/docker-machine-`uname -s`-`uname -m` > /tmp/docker-machine 
+  curl -L https://github.com/docker/machine/releases/download/v0.12.2/docker-machine-`uname -s`-`uname -m` > /tmp/docker-machine 
   sudo mv /tmp/docker-machine /usr/local/bin/docker-machine 
   sudo chmod +x /usr/local/bin/docker-machine
 
