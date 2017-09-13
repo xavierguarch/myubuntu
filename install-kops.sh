2f8c4091e86bd187272d706356247ea2ca636118
Jordi Íñigo <jig@users.noreply.github.com>
2017-09-13 17:47:54 +0200

    Upgraded to kops 1.7.0

--- install-kops.sh
+++ install-kops.sh
@@ -34,7 +34,7 @@ install_kops() {
   echo "source <(kubectl completion bash)" >> ~/.bashrc
   echo "source <(kubectl completion zsh)" >> ~/.zshrc  
   
-  curl -LO https://github.com/kubernetes/kops/releases/download/1.6.0/kops-linux-amd64
+  curl -LO https://github.com/kubernetes/kops/releases/download/1.7.0/kops-linux-amd64
   chmod +x ./kops-linux-amd64
   sudo mv ./kops-linux-amd64 /usr/local/bin/kops
 }
