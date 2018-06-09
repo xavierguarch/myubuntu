7285faa3e0512653c816c0f01aaf7f3f92ec4f5b
Jordi Íñigo <jig@users.noreply.github.com>
2018-06-09 10:33:25 +0200

    Closes #15

--- install-docker.sh
+++ install-docker.sh
@@ -38,6 +38,7 @@ minimal() {
   # Docker Compose i Machine
   printf "${BLUE}Installing docker-compose...${NORMAL}\n"
   sudo curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+  sudo chmod +x /usr/local/bin/docker-compose
   
   printf "${BLUE}Installing docker-machine...${NORMAL}\n"
   curl -L https://github.com/docker/machine/releases/download/latest/docker-machine-`uname -s`-`uname -m` > /tmp/docker-machine 
