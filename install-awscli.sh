09d36a483646903cb8ee0f8f0b94c4efcd530868
Jordi Íñigo <jig@users.noreply.github.com>
2018-06-12 14:24:26 +0200

    Added python to AWScli

--- install-awscli.sh
+++ install-awscli.sh
@@ -25,7 +25,7 @@ awscli() {
   set -e
 
   printf "${BLUE}Installing AWS CLI...${NORMAL}\n"
-  sudo apt-get -y install unzip
+  sudo apt-get -y install unzip python
   curl -JL "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" > /tmp/awscli-bundle.zip
   unzip /tmp/awscli-bundle.zip -d /tmp
   sudo /tmp/awscli-bundle/install -i /usr/local/aws -b /usr/local/bin/aws
