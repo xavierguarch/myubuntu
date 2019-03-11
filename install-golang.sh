394c85c10373bb6c24ad7605468b463aaec9a5c3
Jordi Íñigo <jig@users.noreply.github.com>
2019-03-11 18:27:27 +0100

    Go bumped to 1.12

--- install-golang.sh
+++ install-golang.sh
@@ -27,7 +27,7 @@ golang() {
   ###################################
   # Golang
 
-  VERSION=1.11.5
+  VERSION=1.12
 
   printf "${BLUE}Installing Go...${NORMAL}\n"
   wget https://storage.googleapis.com/golang/go$VERSION.linux-amd64.tar.gz -O /tmp/golang.tgz
